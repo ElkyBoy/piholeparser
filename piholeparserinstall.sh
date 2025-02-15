dd92b05c3cf6c2b82a9c8e7b3b7b3efca5ec80c8
ElkyBoy <99836345+ElkyBoy@users.noreply.github.com>
2025-02-15 22:41:29 +0300

    Update piholeparserinstall.sh

--- piholeparserinstall.sh
+++ piholeparserinstall.sh
@@ -9,7 +9,7 @@
 ## It also sets the variable for the installation
 
 ## Install Variables
-git_repo_name=piholeparser
+git_repo_name=ElkyBoy
 git_repo_owner=deathbybandaid
 git_repo_url_b="https://github.com/$git_repo_owner/$git_repo_name.git"
 git_local_repo_path=/etc/"$git_repo_name"
